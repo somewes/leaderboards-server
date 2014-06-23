0fc48740d7bd45aa09199096b44c6324164db0a4
gustafsonk <kylgus@gmail.com>
2014-06-22 19:54:14 -0500

    route the root directory

--- srllb/urls.py
+++ srllb/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    (r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^leaderboards/', include('leaderboards.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
